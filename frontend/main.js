3a7630f43151dffe426013e38c11de1183119256
danielvera987 <danielveraangulo703@gmail.com>
2020-09-25 15:00:14 -0500

    .

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,11 @@
 window.onload = () => {
- fetch('https://vercel.danielvera987.vercel.app/custom-build/public')
+ fetch('http://localhost:3000/api/meals')
   .then(response => response.json())
-  .then(data => console.log(data))   
+  .then(data => {
+    const mealsList = document.getElementById('meals-list')
+    const submit = document.getElementById('submit')
+    const template = data.map(x => `<li> ${x.name} </li>`).join()
+    mealsList.innerHTML = template
+    submit.removeAttribute('disabled')
+  })   
 }
