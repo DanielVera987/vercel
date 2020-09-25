2e264ba70d4357f16e6cc35b7f201742324282d3
danielvera987 <danielveraangulo703@gmail.com>
2020-09-24 20:37:17 -0500

    Correcion de errores

--- api/routes/meals.js
+++ api/routes/meals.js
@@ -1,12 +1,31 @@
 const express = require('express');
-const router = app.router();
+const Meals = require('../models/Meals');
+
+const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Hola mels desde get')
+  Meals.find()
+      .exec()
+      .then(x => res.status(200).send(x))
+})
+
+router.get('/:id', (req, res) => {
+  Meals.findById(req.params.id)
+       .exec()
+       .then(x => res.status(200).send(x))
 })
 
 router.post('/', (req, res) => {
-  res.send('Hola mels desde post')
+  Meals.create(req.body).then(x => res.status(201).send(x))
+})
+
+router.put('/:id', (req, res) => {
+  Meals.findOneAndUpdate(req.params.id, req.body)
+       .then(() => res.sendStatus(204))
+})
+
+router.delete('/', (req, res) => {
+  Meals.findOneAndDelete(req.params.id).exec().then(() => res.sendStatus(204))
 })
 
 module.exports = router
